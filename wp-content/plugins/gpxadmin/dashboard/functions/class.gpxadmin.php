7e9bd4dc5540f0a11411f5ecb88c7a570b0adad6
rare-wolf <gabeduncan018@gmail.com>
2020-12-21 13:48:42 -0600

    Resort Edit Update

--- wp-content/plugins/gpxadmin/dashboard/functions/class.gpxadmin.php
+++ wp-content/plugins/gpxadmin/dashboard/functions/class.gpxadmin.php
@@ -4395,6 +4395,14 @@ class GpxAdmin {
         $nodates = [
             'ada',
             'attributes',
+            'UnitFacilities',
+            'ResortFacilities',
+            'AreaFacilities',
+            'UnitConfig',
+            'CommonArea',
+            'UponRequest',
+            'UponRequest',
+            'GuestBathroom',
         ];
         
         //$attributeKey is the old date range
