267b0cc715f0f1b6d0886c19f1f7e8ce3aa8343b
rare-wolf <gabeduncan018@gmail.com>
2020-12-30 16:52:35 -0600

    Update promoedit.php

--- wp-content/plugins/gpxadmin/dashboard/templates/admin/promos/promoedit.php
+++ wp-content/plugins/gpxadmin/dashboard/templates/admin/promos/promoedit.php
@@ -359,7 +359,7 @@ foreach($metadates as $metadatekey=>$metadatevalue)
                      <input type="hidden" name="metaUseExc" id="metaUseExc" value="">
                      <div class="usage-exclusion-group">
                      <?php
-                     if(isset($promometa->useExc) && isset($thisisnotset))
+                     if(isset($promometa->useExc))
                      {
                          $pmue = str_replace("\r\n ", "", $promometa->useExc);
                          $pmue = str_replace("\t", "", $pmue);
