b91a4f705dfcc8bc74fe5cf2fe19dad95f01ce74
rare-wolf <gabeduncan018@gmail.com>
2021-01-07 20:02:05 +0530

    change

--- wp-content/plugins/gpxadmin/dashboard/build/js/custom.js
+++ wp-content/plugins/gpxadmin/dashboard/build/js/custom.js
@@ -44,8 +44,6 @@ var CURRENT_URL = window.location.href.split('?')[0], $BODY = jQuery('body'), $M
 
 jQuery( document ).ready( function( $ ) {
 
-alert("sadas");
-
 	jQuery('html body').on('change', '.bootstrap-table-filter-control-check_in_date', function(){
 		var $this = jQuery(this);
 //		jQuery(this).trigger('keyup');
