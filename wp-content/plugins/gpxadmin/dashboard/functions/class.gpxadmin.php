54290670702f560a6982d65525267bdcd2d8925e
rare-wolf <gabeduncan018@gmail.com>
2021-02-03 16:42:36 -0600

    More RW debugging

--- wp-content/plugins/gpxadmin/dashboard/functions/class.gpxadmin.php
+++ wp-content/plugins/gpxadmin/dashboard/functions/class.gpxadmin.php
@@ -2833,6 +2833,7 @@ class GpxAdmin {
                                     $ak = $tk.'.'.$data['usermetaxref'][$t][$ut].'.'.$data['usermetakey'][$t][$ut];
                                     if(isset($_REQUEST['report_debug']))
                                     {
+                                        echo '<pre>'.print_r($data['usermetaxref'], true).'</pre>';
                                         echo '<pre>'.print_r($t, true).'</pre>';
                                         echo '<pre>'.print_r($ut, true).'</pre>';
                                         echo '<pre>'.print_r($ak, true).'</pre>';
