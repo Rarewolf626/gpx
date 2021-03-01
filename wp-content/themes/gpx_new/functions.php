ed1a5e17568f333c0ea0f250a4e414be83cd7854
rare-wolf <gabeduncan018@gmail.com>
2021-03-01 14:07:24 -0600

    Perks

--- wp-content/themes/gpx_new/functions.php
+++ wp-content/themes/gpx_new/functions.php
@@ -5,7 +5,7 @@
  */
 date_default_timezone_set('America/Los_Angeles');
 
-define( 'GPX_THEME_VERSION', '4.017' );
+define( 'GPX_THEME_VERSION', '4.018' );
 
 require_once 'models/gpxmodel.php';
 //$gpx_model = new GPXModel;
