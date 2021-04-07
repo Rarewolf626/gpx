04fee42406fd43b701832788685844955a260c8b
rare-wolf <gabeduncan018@gmail.com>
2021-04-07 22:38:03 +0545

    ajax load testing -- 1486

--- wp-content/themes/gpx_new/pages/page-result.php
+++ wp-content/themes/gpx_new/pages/page-result.php
@@ -7,19 +7,20 @@
 get_header();
 while ( have_posts() ) : the_post();  
 
-the_content();
-print_r($_REQUEST);
+the_content();?>
+<div id="sc-result"></div>
 
+<?php
 endwhile;
 get_footer(); ?>
 <script type="text/javascript">
     $.ajax({
         url: "<?php echo site_url() ?>/wp-admin/admin-ajax.php?action=gpx_ajax_result",
         type: "POST",
-        data: {'action':'gpx_ajax_result','location':'california'},
+        data: {'action':'gpx_ajax_result','location':'<?php echo $_REQUEST['location'] ?>','select_month':'<?php echo $_REQUEST['select_month'] ?>','select_year':'<?php echo $_REQUEST['select_year']; ?>'},
             success:function(res){
 
-                // $('#sc-result').html(res);
+                $('#sc-result').html(res);
         }
         
     })
