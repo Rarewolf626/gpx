0854312369d26c55988028ddfb62f1b96ffda02b
rare-wolf <gabeduncan018@gmail.com>
2021-04-07 22:51:46 +0545

    ajax load testing -- 1486

--- wp-content/themes/gpx_new/functions.php
+++ wp-content/themes/gpx_new/functions.php
@@ -1911,8 +1911,7 @@ function map_dae_to_vest_properties()
     return $output;
 }
 
- 
-add_action('wp_ajax_gpx_ajax_result', 'gpx_result_page_sc');
+  
  
 function gpx_result_page_sc($resortID='', $paginate='', $calendar='')
 {
--- wp-content/themes/gpx_new/pages/page-result.php
+++ wp-content/themes/gpx_new/pages/page-result.php
@@ -8,20 +8,5 @@ get_header();
 while ( have_posts() ) : the_post();  
 
 the_content();
-endwhile;?>
-<div id="sc-result"></div>
-
-<?php get_footer(); ?>
-<script type="text/javascript">
-
-    $.ajax({
-        url: "<?php echo site_url() ?>/wp-admin/admin-ajax.php?action=gpx_ajax_result",
-        type: "POST",
-        data: {'action':'gpx_ajax_result','location':'<?php echo $_REQUEST['location'] ?>'},
-            success:function(res){
-
-                $('#sc-result').html(res);
-        }
-        
-    })
-</script>
+endwhile;
+ get_footer(); ?>
