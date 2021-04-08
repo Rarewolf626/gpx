846a8447c8a52d8b54c5a8177c9b20884b712715
rare-wolf <gabeduncan018@gmail.com>
2021-04-08 08:48:16 -0500

    1488 Cache

--- wp-content/themes/gpx_new/functions.php
+++ wp-content/themes/gpx_new/functions.php
@@ -6,7 +6,7 @@
 date_default_timezone_set('America/Los_Angeles');
 
 
-define( 'GPX_THEME_VERSION', '4.01951' );
+define( 'GPX_THEME_VERSION', '4.01952' );
 
 require_once 'models/gpxmodel.php';
 //$gpx_model = new GPXModel;
