569f79d99b4138a51a35371658e9e67650dfdb64
rare-wolf <gabeduncan018@gmail.com>
2021-04-12 23:51:01 +0545

    1486 ajax result page

--- wp-content/themes/gpx_new/pages/page-result.php
+++ wp-content/themes/gpx_new/pages/page-result.php
@@ -21,7 +21,7 @@ get_header();
             type: "POST",
             data:{'action': 'result_page','location':'<?php echo $_REQUEST['location']; ?>','select_month':'<?php echo $_REQUEST['select_month']; ?>','select_year':'<?php echo $_REQUEST['select_year']; ?>'},
             success:function(res){
-                $('#main-result').html(data);
+                $('#main-result').html(res);
             }
         })
  </script>
