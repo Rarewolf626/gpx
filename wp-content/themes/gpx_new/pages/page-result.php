3847425adf3ea2cc8459f0ed91d86d9d8eaaa1b0
rare-wolf <gabeduncan018@gmail.com>
2021-04-12 23:26:43 +0545

    1486 ajax result page

--- wp-content/themes/gpx_new/pages/page-result.php
+++ wp-content/themes/gpx_new/pages/page-result.php
@@ -15,12 +15,15 @@ print_r($_REQUEST);
 ?>
  <?php get_footer(); ?>
  <script type="text/javascript">
-    $.ajax({
-            url: "<?php echo site_url() ?>/wp-admin/admin-ajax.php?action=result_page",
-            type: "POST",
-            data:{'action': 'result_page','location':'<?php echo $_REQUEST['location']; ?>','select_month':'<?php echo $_REQUEST['select_month']; ?>','select_year':'<?php echo $_REQUEST['select_year']; ?>'},
-            success:function(res){
-                console.log(res);
-            }
-        })
+    $.post('/wp-admin/admin-ajax.php?action=result_page',{location:'<?php echo $_REQUEST['location']; ?>',select_month:'<?php echo $_REQUEST['select_month']; ?>',select_year:'<?php echo $_REQUEST['select_year']; ?>'}, function(data){
+            console.log(data);
+    });		
+    // $.ajax({
+    //         url: "<?php echo site_url() ?>/wp-admin/admin-ajax.php?action=result_page",
+    //         type: "POST",
+    //         data:{'action': 'result_page','location':'<?php echo $_REQUEST['location']; ?>','select_month':'<?php echo $_REQUEST['select_month']; ?>','select_year':'<?php echo $_REQUEST['select_year']; ?>'},
+    //         success:function(res){
+    //             console.log(res);
+    //         }
+    //     })
  </script>
