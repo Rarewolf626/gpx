af68c40e4827732a14d590d95a6f15da8b2b7a2d
rare-wolf <gabeduncan018@gmail.com>
2021-04-23 13:04:39 -0400

    Update sc-result.php

--- wp-content/themes/gpx_new/templates/sc-result.php
+++ wp-content/themes/gpx_new/templates/sc-result.php
@@ -391,7 +391,7 @@ if(isset($loginalert))
                 			     if(!isset($disableMonth))
                 			     {
                 			?>
-                    				<span class="count-result" ><?=count($resort['props'])?> Results</span>
+                    				<span class="count-result" ><?=count($resort['resort'])?> Results</span>
                     				<?php 
                     				if(isset($_POST['select_month']) && !isset($disableMonth))
                     				{
