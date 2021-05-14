8764f008cf440feee021e44ab24e8b7a2030dfda
rare-wolf <gabeduncan018@gmail.com>
2021-05-14 17:22:46 -0500

    Double Hold Again

--- wp-content/themes/gpx_new/functions.php
+++ wp-content/themes/gpx_new/functions.php
@@ -9,7 +9,7 @@
 date_default_timezone_set('America/Los_Angeles');
 
 
-define( 'GPX_THEME_VERSION', '4.0195821' );
+define( 'GPX_THEME_VERSION', '4.019582111' );
 
 require_once 'models/gpxmodel.php';
 //$gpx_model = new GPXModel;
