f96ccb7249b09d11bf020ee4bf8358089de1cd29
rare-wolf <gabeduncan018@gmail.com>
2021-07-26 16:05:04 -0500

    Revert "Cancel Date -- Send to SF"
    This reverts commit 48d2bc59035a43b8363db4c885cfa7396f251f31.

--- wp-content/plugins/gpxadmin/gpxadmin.php
+++ wp-content/plugins/gpxadmin/gpxadmin.php
@@ -7000,7 +7000,6 @@ function hook_credit_import($atts = '')
                                 $sfData['Status__c'] = 'Denied';
                             }
                             $sfData['Reservation_Status__c'] = 'Cancelled';
-                            $sfData['Cancel_Date__c'] = date('Y-m-d');
                             
                             $sfType = 'GPX_Transaction__c';
                             $sfObject = 'GPXTransaction__c';
@@ -10609,7 +10608,6 @@ function gpx_transaction_fees_adjust()
             
             //             $sfData['Purchase_Price__c'] = $updateData['WeekPrice'] += $amount;
             $sfData['Reservation_Status__c'] = 'Cancelled';
-            $sfData['Cancel_Date__c'] = date('Y-m-d');
             $sfData['GPXTransaction__c'] = $transaction;
         }
         if($type == 'discount')
@@ -10732,7 +10730,6 @@ function gpx_transaction_fees_adjust()
             $wpdbUpdate['cancelled'] = 1;
             
             $sfData['Reservation_Status__c'] = 'Cancelled';
-            $sfData['Cancel_Date__c'] = date('Y-m-d');
             $sfData['GPXTransaction__c'] = $transaction;
         }
         
@@ -11218,7 +11215,6 @@ function gpx_cancel_booking($transaction='')
     
     $sfData['Reservation_Status__c'] = 'Cancelled';
     $sfData['GPXTransaction__c'] = $sfTransData->insert->GPXTransaction__c;
-    $sfData['Cancel_Date__c'] = date('Y-m-d');
     //     $sfData['EMS_Account__c'] = $sfTransData->insert->EMS_Account__c;
     //     $sfData['GPX_Ref__c'] = $sfTransData->insert->GPX_Ref__c;
     
