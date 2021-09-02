0305e338d54093b108390607f0587e60e01b67ea
rare-wolf <gabeduncan018@gmail.com>
2021-09-02 15:02:47 -0400

    update for 1486

--- wp-content/themes/gpx_new/templates/sc-booking-path-payment.php
+++ wp-content/themes/gpx_new/templates/sc-booking-path-payment.php
@@ -332,9 +332,7 @@ else
                     $datediff = ($holddate - $holdnow) / 60 / 60;    
                     ?>
                     <div class="hold-limit-countdown" data-limit="<?=$datediff?>">
-                        <?php if(get_option('gpx_hold_limt_timer') !== ''):?>
-                            <h1><?=get_option('gpx_hold_limt_timer')?></h1>
-                        <?php endif;?>
+                        <h1><?=get_option('gpx_hold_limt_timer')?></h1>
                         <div id="clockdiv<?=$prop->id?>" class="show-countdown-timer">
                           <div class="days-div">
                             <span class="days"></span>
