04de1d690f207d79674d429658dcdc03596b6885
rare-wolf <gabeduncan018@gmail.com>
2021-12-08 11:30:23 -0600

    1831 -- Country Adjust

---
+++ wp-content/plugins/gpxadmin/dashboard/templates/admin/regions/regionreassign.php
@@ -0,0 +1,64 @@
+<?php
+
+extract($static);
+extract($data);
+include $dir.'/templates/admin/header.php';
+?>
+        <div class="right_col" role="main">
+          <div class="update-nag"></div>
+          <div class="">
+
+            <div class="page-title">
+              <div class="title_left">
+                <h3>Reassign Region</h3>
+              </div>
+              <div class="title_right text-right">
+              </div>
+            </div>
+                        
+            <div class="clearfix"></div>
+			
+            <div class="row">
+              <div class="col-md-12">
+              <?php if(is_null($RegionID)):?>
+                  <div class="x_content">
+                    <br />
+                    <form id="region-reassign" data-parsley-validate class="form-horizontal form-label-left">
+                    	<div class="form-group">
+                    		<label for="cats">Countries</label>
+                    		<div class="col-sm-6 col-xs-12">
+                    			<select id="cats">
+                    			<?php 
+                    			foreach($cats as $cat)
+                    			{
+                    			?>
+                    				<option value="<?=$cat->id?>"><?=$cat->country?></option>
+                    			<?php 
+                    			}
+                    			?>
+                    			</select>
+                    		</div>
+                    	</div>
+                    	<div class="form-group">
+                    		<label for="cats">Name</label>
+                    		<div class="col-sm-6 col-xs-12">
+                    			<input type="text" name="name" id="ckRegion" class="form-control col-md-7 col-xs-12" />
+                    		</div>
+                    	</div>
+                      <div class="ln_solid"></div>
+                      <div class="form-group">
+                        <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+                          <button type="submit" class="btn btn-success" id="region-submit">Submit <i class="fa fa-circle-o-notch fa-spin fa-fw" style="display: none;"></i></button>
+                        </div>
+                      </div>
+
+                    </form>
+                  </div>  
+              <?php else:?> 
+              	  <h4>This is a base Region.  It cannot be edited, you can only make it a featured region.</h4>            
+              <?php endif;?>
+              </div>
+         	</div>
+         </div>
+       </div>
+       <?php include $dir.'/templates/admin/footer.php';?>
