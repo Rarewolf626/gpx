a14eedf6b2fdc32285ac12f62fb2027ee0a1664f
rare-wolf <gabeduncan018@gmail.com>
2022-01-05 13:31:30 -0600

    1859 Update API Keys

--- wp-content/plugins/gpxadmin/gpxadmin.php
+++ wp-content/plugins/gpxadmin/gpxadmin.php
@@ -26,7 +26,7 @@ if(isset($_REQUEST['debug']))
 }
 
 define( 'GPXADMIN_VERSION', '2.05');
-define("GPX_RECAPTCHA_V3_SECRET_KEY", '6Ldbc-8dAAAAAJpz_bEUDdp2nI0-Bf0CNkbBFzDT');
+define("GPX_RECAPTCHA_V3_SECRET_KEY", '6LeJd_IdAAAAANXngimpxd9j9WbdTVLFaLoDucj_');
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
