af15c700f38639bb2cfdc78a7a31916d32233519
rare-wolf <gabeduncan018@gmail.com>
2022-01-05 15:05:20 -0600

    1859 Add CAPTCHA on both GPXVacations.com and My-GPX.com

--- wp-content/plugins/gpxadmin/gpxadmin.php
+++ wp-content/plugins/gpxadmin/gpxadmin.php
@@ -25,8 +25,8 @@ if(isset($_REQUEST['debug']))
     error_reporting(E_ALL & ~E_NOTICE & ~E_NOTICE & ~E_WARNING);
 }
 
-define( 'GPXADMIN_VERSION', '2.052');
-define("GPX_RECAPTCHA_V3_SECRET_KEY", '6LeJd_IdAAAAANXngimpxd9j9WbdTVLFaLoDucj_');
+define( 'GPXADMIN_VERSION', '2.0521');
+define("GPX_RECAPTCHA_V3_SECRET_KEY", '6LfzhPIdAAAAAJSGo240JqLPJKXdVU5vjrii0Wqm');
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
