41f5f533cb875d9b05f5e12fefeec3b91e28f1c2
rare-wolf <gabeduncan018@gmail.com>
2022-01-06 08:24:33 -0600

    1859 Add CAPTCHA on both GPXVacations.com and My-GPX.com

--- wp-content/themes/gpx_new/functions.php
+++ wp-content/themes/gpx_new/functions.php
@@ -9,7 +9,7 @@
 date_default_timezone_set('America/Los_Angeles');
 
 
-define( 'GPX_THEME_VERSION', '4.10512' );
+define( 'GPX_THEME_VERSION', '4.106' );
 define( 'GPX_RECAPTCHA_KEY', '6LfzhPIdAAAAALbGtjuaU7IX8xfD-dNxvGS0vjQM');
 
 require_once 'models/gpxmodel.php';
