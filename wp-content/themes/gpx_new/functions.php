f39a26478397f5f3ebaaf72da2806bde8f63279e
rare-wolf <gabeduncan018@gmail.com>
2022-03-22 13:47:07 -0700

    increment theme version for cachebusting

--- wp-content/themes/gpx_new/functions.php
+++ wp-content/themes/gpx_new/functions.php
@@ -9,7 +9,7 @@
  */
 date_default_timezone_set( 'America/Los_Angeles' );
 
-define( 'GPX_THEME_VERSION', '4.11' );
+define( 'GPX_THEME_VERSION', '4.12' );
 
 require_once 'models/gpxmodel.php';
 //$gpx_model = new GPXModel;
