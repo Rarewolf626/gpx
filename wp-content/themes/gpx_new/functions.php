825f9d14f51d32e6dafe08305b35718a98cb2019
rare-wolf <gabeduncan018@gmail.com>
2022-04-08 02:12:51 -0700

    Update functions.php
    theme upgrade for cache busting

--- wp-content/themes/gpx_new/functions.php
+++ wp-content/themes/gpx_new/functions.php
@@ -9,7 +9,7 @@
  */
 date_default_timezone_set( 'America/Los_Angeles' );
 
-define( 'GPX_THEME_VERSION', '4.12' );
+define( 'GPX_THEME_VERSION', '4.13' );
 
 require_once 'models/gpxmodel.php';
 //$gpx_model = new GPXModel;
