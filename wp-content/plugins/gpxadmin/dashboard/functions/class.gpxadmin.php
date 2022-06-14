be14318222c2990eb74ef0d9cb02e0c7ca2ff0f1
rare-wolf <gabeduncan018@gmail.com>
2022-06-14 13:08:05 -0700

    TESTING

--- wp-content/plugins/gpxadmin/dashboard/functions/class.gpxadmin.php
+++ wp-content/plugins/gpxadmin/dashboard/functions/class.gpxadmin.php
@@ -8610,8 +8610,10 @@ WHERE
                                         'Hilton Grand Vacations Club at MarBrisa',
                                         'RiverPointe Napa Valley',
                                     ];
+                                    /*
                                     print_r($ownership);
                                     print_r($result->ResortName);
+                                    */
                                     if(in_array($result->ResortName, $selectUnit) || empty($ownership->Room_Type__c))
                                     {
                                         $html .= '<div class="reswrap">';
