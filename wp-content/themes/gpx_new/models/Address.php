1f9576d00ab626009e197a074ebec23c7b8c92b2
rare-wolf <gabeduncan018@gmail.com>
2022-06-30 16:57:13 -0700

    initial start of models
    start of model data structure for code replacement

---
+++ wp-content/themes/gpx_new/models/Address.php
@@ -0,0 +1,68 @@
+<?php
+
+class Address
+{
+    protected string $address1;
+    protected string $address2;
+    protected string $town;
+    protected string $region;
+    protected string $country;
+    protected string $postCode;
+    protected string $phone;
+    protected string $fax;
+    private array $required = array('address1','town','region','country');
+    private string $error = "";
+
+    /**
+     * @throws Exception when required address fields are missing
+     */
+    public function __construct(array $address ){
+
+        if ($this->validate($address)) {
+            $this->address1 = $address['address1'];
+            $this->address2 = $address['address2'] ?? '';
+            $this->town = $address['town'];
+            $this->region = $address['region'] ?? '';
+            $this->country = $address['country'];
+            $this->postCode = $address['postCode'] ?? '';
+            $this->phone = $address['phone'] ?? '';
+            $this->fax = $address['fax'] ?? '';
+        } else {
+            throw new Exception("Required address field missing:".$this->error);
+        }
+
+    }
+
+    private function validate(array $address): bool
+    {
+        foreach ($this->required as $param) {
+            if (!isset($address[$param])) {
+                $this->error = $param;
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /*
+     * create as static method
+     */
+    /**
+     * @throws Exception
+     */
+    public static function create(array ...$parameters){
+        return new static(...$parameters);
+    }
+
+    /*
+     * getter methods
+     */
+    public function get_address1 () { return $this->address1;}
+    public function get_address2 () { return $this->address2;}
+    public function get_town () { return $this->town;}
+    public function get_region () { return $this->region;}
+    public function get_country () { return $this->country;}
+    public function get_postCode () { return $this->postCode;}
+    public function get_phone () { return $this->phone;}
+    public function get_fax () { return $this->fax;}
+}
---
+++ wp-content/themes/gpx_new/models/ResortFee.php
@@ -0,0 +1,65 @@
+<?php
+
+class ResortFee
+{
+    private string $start;
+    private string $end;
+
+    public  $start_date;
+    public  $end_date;
+
+    public float $fee;
+
+    private array $fee_list;
+
+    /*
+     * construct the date and turn into usable data
+     */
+    public function __construct($data) {
+
+        $this->parse_data($data);
+        $this->makedate();
+    }
+
+    /*
+     * input the data sting and split into dates / fees
+     */
+    private function parse_data ($data){
+        $jdata = json_decode($data);
+
+        foreach ($jdata as $key => $times) {
+
+            $this->fee_list = $times;
+            $dates = explode('_',$key,2);
+            $this->start = $dates[0];
+            $this->end = $dates[1];
+
+            $this->fee = ($times[sizeof($times)-1]);
+        }
+    }
+
+    /*
+     * turn the time stamps into actual dates
+     */
+    private function makedate(){
+
+        date_default_timezone_set('America/Los_Angeles');
+       // March 10, 2001  date("F j, Y")
+        $this->start_date = date("F j, Y",$this->start);
+        $this->end_date = date("F j, Y",$this->end);
+    }
+
+    /*
+     * get the resort times in the database format
+     */
+    public function get (){
+        $output = array();
+
+        $output [$this->start.'_'.$this->end] = $this->fee_list;
+
+        return json_encode($output);
+
+    }
+
+
+}
