78736a603f9c0066feb205ccda71ee4cf73a4703
rare-wolf <gabeduncan018@gmail.com>
2022-11-29 11:13:27 -0800

    delete commented out javascript with embeded php that was causing errors

--- wp-content/themes/gpx_new/pages/page-result.php
+++ wp-content/themes/gpx_new/pages/page-result.php
@@ -5,21 +5,12 @@
  */
 
 get_header();
-while ( have_posts() ) : the_post();  
+while ( have_posts() ) : the_post();
 
 the_content();
 endwhile;
 ?>
- 
+
  <?php get_footer(); ?>
- 
- <script type="text/javascript">
-    // $.ajax({
-    //         url: "<?php echo site_url() ?>/wp-admin/admin-ajax.php?action=result_page",
-    //         type: "POST",
-    //         data:{'action': 'result_page','location':'<?php echo $_REQUEST['location']; ?>','select_month':'<?php echo $_REQUEST['select_month']; ?>','select_year':'<?php echo $_REQUEST['select_year']; ?>'},
-    //         success:function(res){
-    //             $('#main-result').html(res);
-    //         }
-    //     })
- </script>
+
+
