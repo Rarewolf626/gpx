196c4545125bdf2ea6349b680ca13e4bc15ae8ee
rare-wolf <gabeduncan018@gmail.com>
2023-01-31 15:57:15 -0800

    update theme version

--- wp-content/themes/gpx_new/functions.php
+++ wp-content/themes/gpx_new/functions.php
@@ -10,7 +10,7 @@ use GPX\Repository\OwnerRepository;
 
 date_default_timezone_set( 'America/Los_Angeles' );
 
-define( 'GPX_THEME_VERSION', '4.25' );
+define( 'GPX_THEME_VERSION', '4.26' );
 
 require_once 'models/gpxmodel.php';
 //$gpx_model = new GPXModel;
