8580af0f4d5bc4423b0c3a03c0a32c9787042d6f
rare-wolf <gabeduncan018@gmail.com>
2023-08-10 15:36:56 -0700

    fix date filter typo in pulling availability

--- wp-content/themes/gpx_new/functions.php
+++ wp-content/themes/gpx_new/functions.php
@@ -1400,8 +1400,6 @@ function gpx_result_page_sc( $resortID = '', $paginate = [], $calendar = '' ) {
 		}
 	}
     $request = wp_unslash($_REQUEST);
-
-    $request = wp_unslash($_REQUEST);
     if ( isset( $request['destination'] ) ) {
         $request['location'] = $request['destination'];
         if ( $request['select_year'] > 2018 ) {
@@ -1410,7 +1408,6 @@ function gpx_result_page_sc( $resortID = '', $paginate = [], $calendar = '' ) {
 			$alldates = true;
 		}
 	}
-
     extract( $request, EXTR_SKIP );
     $select_month = $request['select_month'] ?? '';
 
@@ -1439,9 +1436,9 @@ function gpx_result_page_sc( $resortID = '', $paginate = [], $calendar = '' ) {
 		if ( ( empty( $select_month ) && empty( $select_year ) ) ) {
 			$alldates = true;
 		}
-		if ( mb_strtolower( $select_month ?? '' ) == 'any' ) {
+		if ( mb_strtolower( $select_month ?? 'any' ) == 'any' ) {
 			$thisYear = date( 'Y' );
-			if ( ! isset( $select_year ) ) {
+			if ( empty( $select_year ) ) {
 				$select_year = date( 'Y' );
 			}
 			$monthstart = date( $select_year . '-m-d' );
@@ -1451,11 +1448,11 @@ function gpx_result_page_sc( $resortID = '', $paginate = [], $calendar = '' ) {
 			$monthend = $select_year . "-12-31";
 		} else {
 			$nextmonth = date( 'Y-m-d', strtotime( '+1 month' ) );
-			if ( ! isset( $select_year ) ) {
+			if ( empty( $select_year ) ) {
 				$select_year = date( 'Y' );
 			}
-			if ( ! isset( $select_month ) ) {
-				$select_month = date( 'f', strtotime( $nextmonth ) );
+			if ( empty( $select_month ) ) {
+				$select_month = date( 'F', strtotime( $nextmonth ) );
 			}
 			$monthstart = date( 'Y-m-01', strtotime( $select_month . "-" . $select_year ) );
 			$today = date( 'Y-m-d' );
@@ -1660,7 +1657,6 @@ function gpx_result_page_sc( $resortID = '', $paginate = [], $calendar = '' ) {
 		if ( isset( $limit ) && ! empty( $limit ) ) {
 			$sql .= $limit;
 		}
-
 
 		if ( $resortID || ! empty( $ids ) ) {
 			$props = $wpdb->get_results( $sql );
