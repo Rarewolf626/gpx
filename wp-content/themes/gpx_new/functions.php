8207b58a1d910d13dc3d07cf74843fa9cada4b77
rare-wolf <gabeduncan018@gmail.com>
2024-02-13 15:05:59 -0800

    cache busting

--- wp-content/themes/gpx_new/functions.php
+++ wp-content/themes/gpx_new/functions.php
@@ -120,7 +120,7 @@ if ( ! function_exists( 'load_gpx_theme_styles' ) ) {
 
         wp_enqueue_style( 'daterange-picker', $css_directory_uri . 'daterange-picker.css', [], GPX_THEME_VERSION, 'all' );
         wp_enqueue_style( 'slick-css', 'https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.css', [], GPX_THEME_VERSION, 'all' );
-        wp_enqueue_style( 'ada', $css_directory_uri . 'ada.css', [], '1.1', 'all' );
+        wp_enqueue_style( 'ada', $css_directory_uri . 'ada.css', [], GPX_THEME_VERSION, 'all' );
         wp_enqueue_style( 'ice', $css_directory_uri . 'ice.css', [], GPX_THEME_VERSION, 'all' );
         wp_enqueue_style( 'custom', gpx_asset('custom.css'), ['main'], GPX_THEME_VERSION, 'all' );
     }
